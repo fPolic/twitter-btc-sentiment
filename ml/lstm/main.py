86df25e41ad0bc80db1e6e855c4f0f6958704978
fPolic <frane.polic@fer.hr>
2019-12-19 18:44:08 +0100

    [Data] add csv exports

--- ml/lstm/main.py
+++ ml/lstm/main.py
@@ -1,7 +1,15 @@
+import fire
 import pandas as pd
 import tensorflow as tf
+
+from keras.models import Sequential
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten, LSTM
+from keras.models import Model
+
 import matplotlib.pyplot as plt
 
+from numpy import array
 from pymongo import MongoClient
 
 DATABASE_NAME = 'fer'
@@ -12,6 +20,8 @@ DB = MongoClient(mongoURL)[DATABASE_NAME]
 WINDOW_SIZE = 24
 EMOTIONS = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'negative',
             'positive', 'sadness', 'surprise', 'trust', 'count']
+
+# =================== DATA PREPARATION ===================
 
 
 def getEmotionsDataFrame():
@@ -31,30 +41,76 @@ def getBTCDataFrame():
     return df
 
 
-def calculateZScore(dev, df, column, window=WINDOW_SIZE):
+def calculateZScore(dev, df, column):
     # TODO: CHECK IF JOIN IS CORRECT
-    dev[column] = (df[column] - df[column].rolling(window).mean()
-                   )/df[column].rolling(window).std(ddof=0)
+    dev[column] = (df[column] - df[column].rolling(WINDOW_SIZE).mean()
+                   )/df[column].rolling(WINDOW_SIZE).std(ddof=0)
 
 
 def calculateShareOfEmotion(share, df, column):
     share[column] = (df[column]/df['count']).rolling(WINDOW_SIZE).mean()
 
+# =================== PLOTING TIME SERIES ===================
 
-def plot(share, dev, btc):
 
-    share.plot(x="date")
-    dev.head(200).plot(x="date", subplots=True, layout=(6, 2))
-    btc.plot(x="date", y=["close", 'volume', 'return', 'count'],
-             subplots=True, layout=(4, 1))
+def render(share, dev, btc, plot_args):
+    btc = btc.merge(dev[['count', 'date']], on='date', how='inner')
+
+    if (plot_args == True):
+        share.plot(x="date", figsize=(14, 8),
+                   title='Emotion word count share in total count', fontsize=12)
+        dev.head(200).plot(x="date", subplots=True, layout=(6, 2), figsize=(14, 8),
+                           title='Rolling z-score for 24h period', fontsize=12)
+        btc.plot(x="date", y=["close", 'volume', 'return', 'count'],
+                 subplots=True, layout=(4, 1), figsize=(8, 8),
+                 title='BTC data', fontsize=12)
+
+    if (type(plot_args) != bool):
+        if ('btc' in plot_args):
+            btc.plot(x="date", y=["close", 'volume', 'return', 'count'],
+                     subplots=True, layout=(4, 1), figsize=(8, 8),
+                     title='BTC data', fontsize=12)
+        if ('dev' in plot_args):
+            dev.head(200).plot(x="date", subplots=True, layout=(6, 2), figsize=(14, 8),
+                               title='Rolling z-score for 24h period', fontsize=12)
+        if ('share' in plot_args):
+            share.plot(x="date", figsize=(14, 8),
+                       title='Emotion word count share in total count', fontsize=12)
 
     plt.show()
 
+# =================== LSTM MODEL ===================
 
-def main():
+
+def trainModel(test_data):
+    X = [X[0] for X in test_data]
+    Y = [X[1] for X in test_data]
+
+    T = array(X[-1]).reshape(1, 1, 1)
+    X = array(X[:-1]).reshape(199, 1, 1)
+
+    model = Sequential([
+        LSTM(50, activation='relu', return_sequences=True, input_shape=(1, 1)),
+        LSTM(50, activation='relu'),
+        Dense(1)
+    ])
+
+    model.compile(optimizer='adam', loss='mse', metrics=['mean_squared_error'])
+    model.fit(X, Y[:-1], epochs=500, validation_split=0.2, batch_size=5)
+
+    baseline = pd.DataFrame(Y).tail(24).mean()
+    print(model.predict(T, verbose=True), Y[-1], baseline)
+    return model
+
+
+# =================== MAIN CLI APP ===================
+
+def main(plot=None, train=None, window=WINDOW_SIZE):
+    global WINDOW_SIZE
+    WINDOW_SIZE = window  # optional param overrides default
+
     btc = getBTCDataFrame()
     emotions = getEmotionsDataFrame()
-
     dev = emotions[['date']]
     share = emotions[['date']]
 
@@ -66,24 +122,31 @@ def main():
             continue
         calculateShareOfEmotion(share, emotions, em)
 
-    dev = dev.dropna(axis="rows")
-    # btc = btc.merge(dev[['count', 'date']], on='date', how='inner')
-    # plot(share, dev, btc)
+    dev.dropna(axis="rows", inplace=True)
+    share.dropna(axis="rows", inplace=True)
 
-    test_data = dev.merge(btc, on="date", how="inner")[
-        ['fear', 'return']].head(200).values
+    # btc.to_csv('./colab/btc.csv', index=False)
+    # emotions.to_csv('./colab/emotions.csv', index=False)
+    # dev.to_csv('./colab/dev.csv', index=False)
+    # share.to_csv('./colab/share.csv', index=False)
 
-    # =================== DEFINE MODEL ===================
+    if plot is not None:
+        render(share, dev, btc, plot)
 
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(50, activation='relu', input_shape=(1, 1)),
-        tf.keras.layers.Dense(1)
-    ])
-    model.compile(optimizer='adam', loss='mse')
-    print(model.summary())
+    if train is not None:
 
-    # =================== TRAIN MODEL ===================
+        test_data = dev.merge(btc, on="date", how="inner")[
+            ['fear', 'return']].head(200).values
+
+        model = trainModel(test_data)
+        print(model.summary())
 
 
 if __name__ == "__main__":
-    main()
+    """
+      CLI APP example commands:
+        1. python3 main.py --plot --window=24
+        2. python3 main.py --plot=btc,share
+        3. python3 main.py --train
+    """
+    fire.Fire(main)
