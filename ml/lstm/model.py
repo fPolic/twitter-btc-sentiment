fa1e6d3f1a7bd5f1630cc78057886450f50cdab7
fPolic <frane.polic@fer.hr>
2019-12-21 17:25:54 +0100

    Bidirectional with dropout

--- ml/lstm/model.py
+++ ml/lstm/model.py
@@ -11,21 +11,28 @@ from tensorflow.keras.layers import Flatten, LSTM, Activation, Dropout, Dense, B
 # This 3 params define input shape dimension
 WINDOW_SIZE = 24
 EMOTIONS_DIMENSIONS = 2
-NUMBER_OF_DAYS = 90
+NUMBER_OF_DAYS = 210
 
 
 def getDataInShape(data):
 
     fear = data['fear'].head(NUMBER_OF_DAYS * WINDOW_SIZE).values
-    negative = data['negative'].head(NUMBER_OF_DAYS * WINDOW_SIZE).values
+    count = data['count'].head(NUMBER_OF_DAYS * WINDOW_SIZE).values
 
-    btc_return = data['return'].head(NUMBER_OF_DAYS * WINDOW_SIZE).values
+    btc_return = data['return'].head(NUMBER_OF_DAYS * WINDOW_SIZE)
+    _btc_return = ((btc_return - btc_return.min()) / (btc_return.max() -
+                                                      btc_return.min())).values
 
-    X = array(column_stack((fear, negative))).reshape(
+    # X = []
+    # Y = []
+    # cols = array(column_stack((fear, count)))
+    # for i in range(0, cols.shape[0] - WINDOW_SIZE):
+    #     X.append(cols[i:i + WINDOW_SIZE])
+    #     Y.append(data['close'].values[(i + 1) * WINDOW_SIZE])
+
+    X = array(column_stack((fear, count))).reshape(
         NUMBER_OF_DAYS, WINDOW_SIZE, EMOTIONS_DIMENSIONS)
-
-    # Y = btc_return[::NUMBER_OF_DAYS]
-    Y = add.reduceat(btc_return, arange(0, len(btc_return), WINDOW_SIZE))
+    Y = add.reduceat(_btc_return, arange(0, _btc_return.size, WINDOW_SIZE))
 
     return tf.convert_to_tensor(X, dtype=tf.float32), tf.convert_to_tensor(Y, dtype=tf.float32)
 
@@ -34,7 +41,7 @@ def bidirectional(data):
 
     X, Y = getDataInShape(data)
 
-    X_TEST = tf.reshape(X[-1], (1, 24, 2))
+    X_TEST = tf.reshape(X[-1], (1, WINDOW_SIZE, EMOTIONS_DIMENSIONS))
     Y_TEST = Y[-1]
 
     X = X[:-1]
@@ -47,13 +54,34 @@ def bidirectional(data):
     model = Sequential([
         Bidirectional(LSTM(WINDOW_SIZE, activation='relu',
                            return_sequences=True), input_shape=(WINDOW_SIZE, EMOTIONS_DIMENSIONS)),
+        Dropout(0.4),
         Bidirectional(LSTM(WINDOW_SIZE, activation='relu')),
         Dense(1)
     ])
-    model.compile(optimizer='adam', loss='mse', metrics=['mean_squared_error'])
-    model.fit(X, Y, epochs=1000, validation_split=0.2,
-              batch_size=WINDOW_SIZE, callbacks=[tensorboard_callback])
-    print(model.summary())
+
+    # model = Sequential()
+    # model.add(LSTM(200, activation='relu',
+    #                return_sequences=True, input_shape=(WINDOW_SIZE, 2)))
+    # model.add(LSTM(100, activation='relu', return_sequences=True))
+    # model.add(LSTM(50, activation='relu', return_sequences=True))
+    # model.add(LSTM(25, activation='relu'))
+    # model.add(Dense(20, activation='relu'))
+    # model.add(Dropout(0.2))
+    # model.add(Dense(10, activation='relu'))
+    # model.add(Dense(1))
+
+    model.compile(optimizer='adam', loss='mse', metrics=[
+                  'mean_squared_error'])
+    history = model.fit(X, Y, epochs=500, validation_split=0.2,
+                        batch_size=WINDOW_SIZE, callbacks=[tensorboard_callback])
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model train vs validation loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper right')
+    plt.show()
 
     baseline = data['return'].tail(
         WINDOW_SIZE * EMOTIONS_DIMENSIONS * NUMBER_OF_DAYS).mean()
