532e973f203e66bf6009165d6da47955bfc872b1
fPolic <frane.polic@fer.hr>
2020-01-17 21:40:31 +0100

    LSTM shape for abs volatility

--- ml/app/models/lstm.py
+++ ml/app/models/lstm.py
@@ -14,8 +14,8 @@ from tensorflow.keras.layers import Flatten, LSTM, Activation, Dropout, Dense, B
 from utils.dataset_split import split_dataframe
 
 # This 3 params define input shape dimension
-WINDOW_SIZE = 24
-EMOTIONS_DIMENSIONS = 4 + 1
+WINDOW_SIZE = 1
+EMOTIONS_DIMENSIONS = 11
 NUMBER_OF_DAYS = WINDOW_SIZE * 30
 
 
@@ -24,34 +24,32 @@ def getDataInShape(data):
 
     X_train.head(NUMBER_OF_DAYS * WINDOW_SIZE)
 
-    positive = data['positive'].head(NUMBER_OF_DAYS * WINDOW_SIZE).values
-    negative = data['negative'].head(NUMBER_OF_DAYS * WINDOW_SIZE).values
-    trust = data['trust'].head(NUMBER_OF_DAYS * WINDOW_SIZE).values
-    anticipation = data['anticipation'].head(
-        NUMBER_OF_DAYS * WINDOW_SIZE).values
+    anger = X_train['anger']
+    anticipation = X_train['anticipation']
+    disgust = X_train['disgust']
+    fear = X_train['fear']
+    joy = X_train['joy']
+    negative = X_train['negative']
+    positive = X_train['positive']
+    sadness = X_train['sadness']
+    surprise = X_train['surprise']
+    trust = X_train['trust']
+    return_ = X_train['return']
 
-    btc_close = data['close'].head(
-        NUMBER_OF_DAYS * WINDOW_SIZE).values
+    Y = y_train['target'].values
+    X = array(column_stack(
+        (anger, anticipation, disgust, fear, joy, negative, positive, sadness, surprise, trust, return_)))
 
-    btc_return = data['return'].head(
-        NUMBER_OF_DAYS * WINDOW_SIZE + WINDOW_SIZE).values[WINDOW_SIZE:]
+    X = X.reshape(
+        (X.shape[0], X.shape[1], 1))
 
-    X = []
-    Y = []
-    cols = array(column_stack(
-        (positive, negative, trust, anticipation, btc_close)))
-    for i in range(0, cols.shape[0] - WINDOW_SIZE):
-        X.append(cols[i:i + WINDOW_SIZE])
-        Y.append(functools.reduce(operator.add,
-                                  btc_return[i: i + WINDOW_SIZE]))
-
-    return tf.convert_to_tensor(X, dtype=tf.float32), tf.convert_to_tensor(Y, dtype=tf.float32)
+    return tf.convert_to_tensor(X, dtype=tf.float32), tf.convert_to_tensor(Y, dtype=tf.float32), X_test, y_test['target'].values
 
 
 def bidirectional():
     return Sequential([
         Bidirectional(LSTM(EMOTIONS_DIMENSIONS, activation='relu',
-                           return_sequences=True), input_shape=(WINDOW_SIZE, EMOTIONS_DIMENSIONS)),
+                           return_sequences=True), input_shape=(EMOTIONS_DIMENSIONS, WINDOW_SIZE)),
         # Dropout(0.3),
         Bidirectional(LSTM(EMOTIONS_DIMENSIONS, activation='relu')),
         Dropout(0.2),
@@ -96,7 +94,7 @@ def train(data, lstm_type='bidirectional', plot_loss=True):
     model.compile(optimizer='adam', loss='mse', metrics=['mean_squared_error'])
 
     history = model.fit(X, Y, epochs=100, validation_split=0.2,
-                        batch_size=WINDOW_SIZE * 7, callbacks=[tensorboard_callback])
+                        batch_size=WINDOW_SIZE * 24, callbacks=[tensorboard_callback])
 
     if (plot_loss):
         plotLoss(history)
