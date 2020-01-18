0bb74f951780c0520a2bc0b243c656d4c83c395f
fPolic <frane.polic@fer.hr>
2020-01-18 16:54:18 +0100

    Change model params, add volume as feature and test seasonal features such as houre of day

--- ml/app/models/xgboost__.py
+++ ml/app/models/xgboost__.py
@@ -13,52 +13,82 @@ from matplotlib import pyplot
 from utils.dataset_split import split_dataframe
 
 
-def train(data):
-
-    X_train, y_train, X_test, y_test = split_dataframe(data)
+def train_linear(data, fig):
+    X_train, y_train, X_test, y_test = split_dataframe(
+        data, model_type="linreg")
 
     lin = LinearRegression()
     lin.fit(X_train, y_train['target'])
 
-    reg = xgb.XGBRegressor(n_estimators=1000)
-    reg.fit(X_train, y_train,
-            eval_set=[(X_train, y_train), (X_test, y_test)],
-            early_stopping_rounds=50,
-            verbose=False)
-
-    pred = reg.predict(X_test)
     pred_lin = lin.predict(X_test)
-    print(X_train.shape, y_test.shape)
 
-    X_test['pred'] = pred
-
-    xgb_r2 = "XGBoost r2: " + \
-        str(around(r2_score(y_test['target'], pred), decimals=4))
-    lin_r2 = "Lin. reg. r2: " + \
+    lin_r2 = "Lin. reg. R2: " + \
         str(around(r2_score(y_test['target'], pred_lin), decimals=4))
 
-    print("XGBoost feature importance: ", reg.get_booster().get_score())
-    print(xgb)
-    print(lin_r2)
-
-    # ==================== PLOT RESULTS & FEATURES IMPORTANCE ===================
-
-    fig = make_subplots(shared_xaxes=False, rows=2, cols=1)
-    fig.add_scatter(
-        x=X_test.index, y=y_test['target'], marker_color=y_test['target'], mode='lines', name='BTC absolute returns', row=1, col=1)
-    fig.add_scatter(
-        x=X_test.index, y=X_test['pred'], mode='lines', name='XGBoost predicted', row=1, col=1)
     fig.add_scatter(x=X_test.index, y=pred_lin, mode='lines',
                     name='Linear reg. predicted', row=1, col=1)
 
     fig.add_trace(go.Scatter(
-        x=[X_test.index[150], X_test.index[150]],
-        y=[0.077, 0.067],
+        x=[X_test.index[100]],
+        y=[0.075],
         mode='text',
-        name='R2 score',
-        text=[lin_r2, xgb_r2],
-        textposition='bottom center'
+        text=[lin_r2],
+        name='Linear reg. R2',
+        textposition='bottom right'
     ))
+
+    print(lin_r2)
+
+
+def train_xgboost(data, fig):
+    X_train, y_train, X_test, y_test = split_dataframe(data)
+
+    reg = xgb.XGBRegressor(
+        max_depth=8,
+        n_estimators=1000,
+        min_child_weight=300,
+        colsample_bytree=0.8,
+        subsample=0.8,
+        eta=0.3,
+        seed=50)
+
+    reg.fit(X_train, y_train,
+            eval_set=[(X_train, y_train), (X_test, y_test)],
+            early_stopping_rounds=10, verbose=False)
+
+    pred = reg.predict(X_test)
+
+    print(X_train.shape, y_test.shape)
+
+    X_test['pred'] = pred
+
+    xgb_r2 = "XGBoost R2: " + \
+        str(around(r2_score(y_test['target'], pred), decimals=4))
+
+    print("XGBoost feature importance: ", reg.get_booster().get_score())
+    print(xgb_r2)
+
+    fig.add_scatter(
+        x=X_test.index, y=y_test['target'], marker_color=y_test['target'], mode='lines', name='BTC absolute returns', row=1, col=1)
+    fig.add_scatter(
+        x=X_test.index, y=X_test['pred'], mode='lines', name='XGBoost predicted', row=1, col=1)
+
+    fig.add_trace(go.Scatter(
+        x=[X_test.index[100]],
+        y=[0.067],
+        mode='text',
+        text=[xgb_r2],
+        name='XGBoost R2',
+        textposition='bottom right'
+    ))
+
+
+def train(data):
+
+    fig = make_subplots(shared_xaxes=False, rows=2, cols=1)
+
+    train_linear(data.copy(), fig)
+    train_xgboost(data.copy(), fig)
 
     fig.update_layout(title_text="Model performances",
                       legend_orientation="v", height=900)
--- ml/app/utils/dataset_split.py
+++ ml/app/utils/dataset_split.py
@@ -4,23 +4,46 @@ __SPLIT_DATE__ = '2018-09-30'
 # FROM_DATA = '2018-09-02'
 
 
-def split_dataframe(data, SPLIT_DATE=__SPLIT_DATE__):
+def process_df_for_linreg(data):
+    data['return'] = data['return'].abs()
+    data['volume'] = data['volume'].diff()
+    data['hour'] = data.index.hour
+
+    data['target'] = data['return'].shift(-1)
+
+    # data['dayOfWeek'] = data.index.dayofweek
+    # data['dayOfMonth'] = data.index.day
+
+    data.dropna(axis="rows", inplace=True)
+
+
+def process_df_for_xgboost(data):
+    data['return'] = data['return'].abs()
+    data['volume'] = data['volume']
+    data['hour'] = data.index.hour
+
+    data['target'] = data['return'].shift(-1)
+
+    # data['dayOfWeek'] = data.index.dayofweek
+    # data['dayOfMonth'] = data.index.day
+
+    data.fillna(0, inplace=True)
+
+
+def split_dataframe(data, model_type="xgboost", SPLIT_DATE=__SPLIT_DATE__):
     # data -> Pandas dataframe indexed by date
 
     # data = data.loc[FROM_DATA:]
 
-    data['return'] = data['return'].abs()
-    data['target'] = data['return'].shift(-1)
-    data['volume'] = data['volume'].diff()
-
-    data.dropna(axis="rows", inplace=True)
-    print(data.head())
+    if model_type == 'linreg':
+        process_df_for_linreg(data)
+    else:
+        process_df_for_xgboost(data)
 
     train = data.loc[data.index < SPLIT_DATE]
     test = data.loc[data.index > SPLIT_DATE]
 
-    FEATURES = ['return',  'volume',  'anticipation',  'negative', 'sadness']
-
+    FEATURES = ['return',  'volume',  'anticipation', 'sadness', 'hour']
     TARGET = ['target']
 
     X_train = train[FEATURES]
